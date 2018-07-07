41033d73bcf38f7f2426afc0cd57c7f32d7b16d4
wiztemple <wiztemple7@gmail.com>
2018-07-07 10:32:16 +0100

    set up server

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'volleyball';
+import dotenv from  'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 7000;
+// Log Requests
+app.use(logger);
+
+// App body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => {
+    return response.status(200).json({
+        status: 'success',
+        message: 'Welcome To Diarium... Your emotions matter, Write it down!'
+    })
+})
+app.listen(port, () => {
+    console.log(`Diarium listening on port ${port}`)
+})
+export default app;
