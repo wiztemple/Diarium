67a52c5009605f095657b2b944524e15f267a02b
wiztemple <wiztemple7@gmail.com>
2018-07-07 10:10:58 +0100

    profile page

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -6,3 +6,32 @@ window.onscroll = function () {
     footer.style.display = 'none';
   }
 };
+
+let modal = document.getElementById('simpleModal');
+
+let modalBtn = document.getElementById('modalBtn');
+
+let closeBtn = document.querySelector('.closeBtn');
+
+// listen for open click
+modalBtn.addEventListener('click', openModal);
+
+// listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+//listen for outside click
+window.addEventListener('click', clickOutSide);
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+function clickOutSide(e){
+  if(e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
